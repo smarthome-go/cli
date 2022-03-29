539bfa8c4e337bae8f58038f7882406255c8013e
Mik Mueller <83001409+MikMuellerDev@users.noreply.github.com>
2022-03-29 23:19:02 +0200

    Remove column names in switch listing

--- cmd/builtin.go
+++ cmd/builtin.go
@@ -7,7 +7,9 @@ import (
 )
 
 func listSwitches() {
-	log.Logn("\u2502Id          \u2502 Label	            \u2502   Room     \u2502 Watts")
+	// log.Logn("\u2502Id          \u2502 Label	            \u2502   Room     \u2502 Watts")
+	// TODO: add power indicator
+	log.Logn("\x1b[90m(Id, Label, Room, Watts)\x1b[0m")
 	for _, switchItem := range Switches {
 		log.Logn(fmt.Sprintf("\u2502%-10s  \u2502 %-20s \u2502 %10s \u2502 %d", switchItem.Id, switchItem.Name, switchItem.RoomId, switchItem.Watts))
 	}

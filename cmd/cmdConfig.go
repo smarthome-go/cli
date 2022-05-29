81230b9ccdac5b6581b228c351a901d97d34555b
Mik Mueller <83001409+MikMuellerDev@users.noreply.github.com>
2022-05-29 14:50:20 +0200

    Moved large subcommands from `cmd/root.go` to different files

---
+++ cmd/cmdConfig.go
@@ -0,0 +1,77 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+func createCmdConfig() *cobra.Command {
+	// Parent configuration commands
+	cmdConfig := &cobra.Command{
+		Use:   "config",
+		Short: "CLI configuration",
+		Long:  "Retrieve and update the CLI configuration. If no arguments are provided, the configuration is printed. The configuration can be updated with [Username, Password, SmarthomeURL]",
+		Args:  cobra.NoArgs,
+		Run: func(cmd *cobra.Command, args []string) {
+			if err := cmd.Help(); err != nil {
+				panic(err.Error())
+			}
+			printConfig()
+		},
+	}
+
+	// View current configuration
+	cmdConfigGet := &cobra.Command{
+		Use:   "get",
+		Short: "View configuration",
+		Long:  "View the parameters which are currently stored in the configuration file.",
+		Args:  cobra.NoArgs,
+		PreRun: func(cmd *cobra.Command, args []string) {
+			readConfigFile()
+		},
+		Run: func(cmd *cobra.Command, args []string) {
+			printConfig()
+		},
+	}
+	cmdConfig.AddCommand(cmdConfigGet)
+
+	// Delete configuration
+	cmdConfigRm := &cobra.Command{
+		Use:   "rm",
+		Short: "Remove configuration",
+		Long:  "Deletes the configuration file from the filesystem",
+		Run: func(cmd *cobra.Command, args []string) {
+			deleteConfigFile()
+		},
+	}
+	cmdConfig.AddCommand(cmdConfigRm)
+
+	// Update configuration
+	setUsername := ""
+	setPassword := ""
+	setURL := ""
+	cmdConfigSet := &cobra.Command{
+		Use:   "set",
+		Short: "Update configuration",
+		Long:  "Write new configuration values to the configuration file",
+		PreRun: func(cmd *cobra.Command, args []string) {
+			readConfigFile()
+		},
+		Run: func(cmd *cobra.Command, args []string) {
+			if setPassword == "" && setUsername == "" && setURL == "" {
+				fmt.Println("Provided at least one of the flags below in order to update the configuration.")
+				if err := cmd.Help(); err != nil {
+					panic(err.Error())
+				}
+				return
+			}
+			writeConfig(setUsername, setPassword, setURL)
+		},
+	}
+	cmdConfigSet.Flags().StringVarP(&setUsername, "new-username", "n", "", "username to be updated")
+	cmdConfigSet.Flags().StringVarP(&setPassword, "new-password", "t", "", "password to be updated")
+	cmdConfigSet.Flags().StringVarP(&setURL, "new-ip", "a", "", "url / ip to be updated")
+	cmdConfig.AddCommand(cmdConfigSet)
+	return cmdConfig
+}

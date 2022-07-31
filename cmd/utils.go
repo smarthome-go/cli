c92c39abfbb399a39c80cc57bf5fd490e2d7da22
Mik Mueller <83001409+MikMuellerDev@users.noreply.github.com>
2022-07-31 19:03:50 +0200

    Fix typo in utils.go

--- cmd/utils.go
+++ cmd/utils.go
@@ -14,7 +14,7 @@ func processHmsArgs(args []string) (map[string]string, error) {
 		case 1:
 			argsTemp[strings.Split(arg, ":")[0]] = strings.Split(arg, ":")[1]
 		default:
-			return nil, fmt.Errorf("Bad Homescript argument formatting at postition %d: '%s' contains more than one ':' separator.\nThe separator is required in order to distinguish between key and value", indexArg, arg)
+			return nil, fmt.Errorf("Bad Homescript argument formatting at position %d: '%s' contains more than one ':' separator.\nThe separator is required in order to distinguish between key and value", indexArg, arg)
 		}
 	}
 	return argsTemp, nil
